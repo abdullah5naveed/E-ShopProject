0ea5d0dafdbd96a8a03db08127188ea5f3f441df
abdullah5naveed <abdullah5naveed@gmail.com>
2020-11-20 09:28:22 +0500

    Add Models

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Product
+from .models import Category
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+
+
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category)
+
+
--- store/models.py
+++ store/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.name
+
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.FloatField(default=0)
+    quantity = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.CharField(max_length=150, default="")
+    image = models.ImageField(upload_to="media/product/img/")
+
+    @staticmethod
+    def get_all_products():
+        return Product.objects.all()
+
+
